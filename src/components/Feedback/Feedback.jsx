119a566d9136b9cdb182b90d1927359649e71021
Sergio Nezhigay <abc1971h@gmail.com>
2023-04-11 15:34:27 +0300

    rem'd unused var

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 import { Title, Row, Button, Info, List, Item } from './Feedback.styled';
 
